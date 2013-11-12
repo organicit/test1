de028ae7d38db087a972004f70306243a616dc1a
Eric Courville <orgnaicit-git@outlook.com>
2013-11-11 19:58:46 -0800

    initial commit

---
+++ load-azureenv.ps1
@@ -0,0 +1,8 @@
+﻿<#
+	Setup Demo Environment
+	
+#>
+Import-Module 'C:\Program Files (x86)\Microsoft SDKs\Windows Azure\PowerShell\Azure\Azure.psd1'
+Set-AzureSubscription 'C:\Users\Eric\Downloads\Visual Studio Professional with MSDN-11-12-2013-credentials.publishsettings'
+Select-AzureSubscription "Visual Studio Professional with MSDN"
+
---
+++ setenv.bat
@@ -0,0 +1,1 @@
+﻿C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe /E:ON /V:ON /K "'C:\Program Files\Microsoft SDKs\Windows Azure\.NET SDK\v.2.2\\bin\setenv.cmd"
